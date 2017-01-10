8839a7d4567b1499f6bbe15bced5577372b8e2ce
Taylor McCarthy <oresshi@gmail.com>
2017-01-10 15:41:18 -0700

    Added table.

--- index.php
+++ index.php
@@ -4,5 +4,45 @@
 		<meta charset="utf-8" />
 		<title>HTML SANDBOX</title>
 	</head>
-	<body></body>
+	<body>
+		<header>
+			<h1>This is a is a is a is a is a is a is a is a is a is a is a is a is a is a is a is a is a is a is a...</h1>
+		</header>
+		<p>Shizzle my nizzle crocodizzle ipsizzle dolizzle sizzle amizzle, tellivizzle adipiscing get down get down. Dizzle sapizzle velizzle, get down get down volutpizzle, suscipizzle quizzle, bow wow wow vizzle, arcu. Pellentesque sizzle tortor. Mammasay mammasa mamma oo sa bizzle. Fusce izzle dolor dapibizzle turpis tempizzle tempor. Mauris pellentesque fo shizzle mah nizzle fo rizzle, mah home g-dizzle izzle shiz. Shiznit ma nizzle tortizzle. Pellentesque eleifend izzle shizzle my nizzle crocodizzle. Black gangster ass platea dictumst. Owned dapibizzle. I'm in the shizzle tellus urna, pretium eu, mattis yo, izzle vitae, shit. Fo suscipizzle. Integer sempizzle velit sheezy fo shizzle. Fo shizzle mah nizzle fo rizzle, mah home g-dizzle dope mauris egizzle nisi bow wow wow pretium. Vivamizzle sit amet its fo rizzle. Tellivizzle eu nisl eget lacizzle auctizzle own yo'. Fo shizzle suscipizzle ipsum. Curabitur dope arcu. Fo shizzle my nizzle enizzle mofo, auctizzle nizzle, congue gizzle, you son of a bizzle shiznit, libero. Fo vitae my shizz nizzle tellivizzle posuere fizzle. Quisque get down get down shiz, congue pulvinizzle, auctor a, mollis sizzle amizzle, erizzle. Sure izzle dui. Aliquizzle purizzle, elementum go to hizzle, sollicitudin in, away imperdizzle, dizzle. Quisque stuff ipsum crunk mi mofo vehicula. Curabitur break it down things ipsizzle. We gonna chung that's the shizzle morbi the bizzle senectus yo netus da bomb hizzle famizzle shiznit turpizzle fo. In est. Crazy sizzle. Ut break it down felizzle, semper quis, suscipit ac, phat dizzle, izzle. Nulla sagittizzle gravida velit. Own yo' ut dolor. Fizzle magna crazy, dignissim mofo amizzle, check it out izzle, sollicitudin nizzle, tellivizzle. Dawg shizzlin dizzle nisi. Sed malesuada brizzle mah nizzle risizzle. Suspendisse own yo'. Shit dope tristique turpizzle. Suspendisse sure boom shackalack my shizz. Sizzle dolizzle libero, fo shizzle interdizzle, cool ass, dapibus izzle, stuff. Suspendisse away crazy. Dang aliquizzle gangster sizzle amet phat. Fusce pot felis izzle ma nizzle. Fo ipsizzle dolizzle sit amet, consectetuer away elit. Fo shizzle mah nizzle fo rizzle, mah home g-dizzle fo purizzle, eleifend laoreet, uhuh ... yih! izzle, shut the shizzle up nizzle, shit. Nizzle porta cool nunc. Nunc doggy lorizzle, get down get down, i saw beyonces tizzles and my pizzle went crizzle egizzle, dope id, dizzle. Etizzle yo mamma leo a sem owned mattizzle. My shizz interdizzle gangster izzle eros own yo' gangster. Etiam things fermentum ligula. Morbi . Maecenas that's the shizzle metus ac dolizzle iaculis auctizzle. Integer sagittizzle viverra urna. Pizzle sollicitudizzle boom shackalack quizzle purus. Morbi crazy ligula sizzle amizzle tellus. Crazy ghetto enizzle vitae arcu. The bizzle sociis penatibus rizzle magnis dis parturient shut the shizzle up, nascetizzle for sure tellivizzle.</p>
+		<h2>Some Things</h2>
+		<!-- ALL THE COMMENTS! -->
+		<ul>
+			<li>Cats</li>
+			<li>Bowling</li>
+			<li>Tacos</li>
+			<li>Tow Truck</li>
+		</ul>
+		<h3>
+			<table>
+				<!-- Row 1 -->
+				<tr>
+					<th>Column 1</th>
+					<th>Column 2</th>
+				</tr>
+
+				<!-- Row 2 -->
+				<tr>
+					<td>Tabby</td>
+					<td>Beagle</td>
+				</tr>
+
+				<!-- Row 3 -->
+				<tr>
+					<td>Persian</td>
+					<td>Bulldog</td>
+				</tr>
+
+				<!-- Row 4 -->
+				<tr>
+					<td>Siamese</td>\
+					<td>Boxer</td>
+				</tr>
+			</table>
+		</h3>
+	</body>
 </html>
