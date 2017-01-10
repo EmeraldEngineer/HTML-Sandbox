b37cad8260c492251168bfe8853f14d80f58363d
Taylor McCarthy <oresshi@gmail.com>
2017-01-10 15:46:49 -0700

    Added table.

--- index.php
+++ index.php
@@ -17,7 +17,7 @@
 			<li>Tacos</li>
 			<li>Tow Truck</li>
 		</ul>
-			<table>
+		<table>
 				<!-- Row 1 -->
 				<tr>
 					<th>Column 1</th>
@@ -41,7 +41,7 @@
 					<td>Siamese</td>\
 					<td>Boxer</td>
 				</tr>
-			</table>
+		</table>
 
 	</body>
 </html>
